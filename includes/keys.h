a20edd151a55990942253346a32c2e76df7181ec
Alainml <alan.mirlo@gmail.com>
2022-11-19 09:56:12 +0100

    .h keys and graphics

---
+++ includes/keys.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keys.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almirand <almirand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 09:42:25 by almirand          #+#    #+#             */
+/*   Updated: 2022/11/19 09:51:36 by almirand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEYS_H
+# define KEYS_H
+
+/*
+** Movement ASWD
+*/
+
+# define A_L 0
+# define S_D 1
+# define D_R 2
+# define W_U 13
+
+/*
+** Movement ARROWS
+*/
+
+# define AR_L 123
+# define AR_D 125
+# define AR_R 124
+# define AR_U 126
+
+#endif
